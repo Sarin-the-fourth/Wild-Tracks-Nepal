4dcc89c145e555cea5e32c7841be048a70e1ad45
Sarin <joshisarinatwork@gmail.com>
2025-12-03 21:49:00 +0545

    axios

--- client_side/src/assets/axios.js
+++ client_side/src/assets/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: import.meta.env.VITE_BACKEND_API,
   withCredentials: true,
 });
